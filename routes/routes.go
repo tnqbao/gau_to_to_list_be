e5cbbdfac19a3689b7c35f0304755435c4c3a8b8
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2025-02-21 20:37:22 +0700

    update cmd, add create-task-endpoint (POST /task)

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/tnqbao/gau-to-do-list/api/task"
 	"github.com/tnqbao/gau-to-do-list/middlewares"
 )
 
@@ -9,5 +10,9 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.Use(middlewares.CORSMiddleware())
 
+	endpoint := r.Group("/")
+	{
+		endpoint.POST("/task", task.CreateTask)
+	}
 	return r
 }
