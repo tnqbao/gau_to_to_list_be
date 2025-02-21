677be6a148855f4c4aaf77464471ac881c986e53
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2025-02-21 20:39:19 +0700

    update create-task api

--- api/task/create.task.go
+++ api/task/create.task.go
@@ -1,7 +1,19 @@
 package task
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tnqbao/gau-to-do-list/models"
+)
 
 func CreateTask(c *gin.Context) {
-	
+	listTask := c.MustGet("listTask").([]models.Task)
+	var task models.Task
+	if err := c.BindJSON(&task); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	listTask = append(listTask, task)
+	c.JSON(200, gin.H{"message": "Task created!", "task": task})
+
 }
